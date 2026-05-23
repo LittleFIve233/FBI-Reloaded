6cb3209333449299bd570404d2aafdc4f6f7eaf2
林 三月 <68946023+LittleFIve233@users.noreply.github.com>
2026-05-23 22:50:20 +0800

    Update file paths for lasturls in remoteinstall.c

--- source/fbi/remoteinstall.c
+++ source/fbi/remoteinstall.c
@@ -21,7 +21,7 @@ static bool remoteinstall_get_last_urls(char* out, size_t size) {
     }
 
     Handle file = 0;
-    if(R_FAILED(FSUSER_OpenFileDirectly(&file, ARCHIVE_SDMC, fsMakePath(PATH_EMPTY, ""), fsMakePath(PATH_ASCII, "/fbi/lasturls"), FS_OPEN_READ, 0))) {
+    if(R_FAILED(FSUSER_OpenFileDirectly(&file, ARCHIVE_SDMC, fsMakePath(PATH_EMPTY, ""), fsMakePath(PATH_ASCII, "/3ds/fbi/lasturls"), FS_OPEN_READ, 0))) {
         return false;
     }
 
@@ -39,7 +39,7 @@ static Result remoteinstall_set_last_urls(const char* urls) {
 
     FS_Archive sdmcArchive = 0;
     if(R_SUCCEEDED(res = FSUSER_OpenArchive(&sdmcArchive, ARCHIVE_SDMC, fsMakePath(PATH_EMPTY, "")))) {
-        FS_Path path = fsMakePath(PATH_ASCII, "/fbi/lasturls");
+        FS_Path path = fsMakePath(PATH_ASCII, "/3ds/fbi/lasturls");
 
         Handle file = 0;
         if(R_SUCCEEDED(FSUSER_OpenFile(&file, sdmcArchive, path, FS_OPEN_READ, 0))) {
@@ -50,7 +50,7 @@ static Result remoteinstall_set_last_urls(const char* urls) {
 
         if(urls != NULL && strlen(urls) != 0
            && R_SUCCEEDED(res)
-           && R_SUCCEEDED(res = fs_ensure_dir(sdmcArchive, "/fbi/"))
+           && R_SUCCEEDED(res = fs_ensure_dir(sdmcArchive, "/3ds/fbi/"))
            && R_SUCCEEDED(res = FSUSER_OpenFile(&file, sdmcArchive, path, FS_OPEN_WRITE | FS_OPEN_CREATE, 0))) {
             u32 bytesWritten = 0;
             res = FSFILE_Write(file, &bytesWritten, 0, urls, strlen(urls), FS_WRITE_FLUSH | FS_WRITE_UPDATE_TIME);
